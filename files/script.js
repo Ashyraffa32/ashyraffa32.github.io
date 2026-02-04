cd6ec8d62b4c2f628825f0e88ae338ba1efd622b
Ashyraffa32 <raffadandilla@gmail.com>
2026-02-04 21:11:08 +0700

    complete change

---
+++ files/script.js
@@ -0,0 +1,34 @@
+
+    function checkFont(fontName) {
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        const text = "abcdefghijklmnopqrstuvwxyz0123456789";
+        
+        context.font = "72px monospace";
+        const baselineWidth = context.measureText(text).width;
+        
+        context.font = "72px '" + fontName + "', monospace";
+        const newWidth = context.measureText(text).width;
+        
+        return newWidth !== baselineWidth;
+    }
+
+    // If the font ISN'T detected, show the warning
+    if (!checkFont("OMORI_GAME")) {
+        document.getElementById("font-warning").style.display = "block";
+    }
+
+    // Switcher
+    function showPage(pageId) {
+    // Hide all sections
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => {
+        page.style.display = 'none';
+    });
+
+    // Show the requested section
+    const selectedPage = document.getElementById(pageId);
+    if (selectedPage) {
+        selectedPage.style.display = 'block';
+    }
+}
